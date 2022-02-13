e6a9b9c27379a47f2b7d3b75f1f1715923c14448
mssalkhalifah <mssalkhalifah@gmail.com>
2022-02-13 14:17:45 +0300

    nextjs config edit

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  //assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: 'mssalkhalifah.github.io/',
 }

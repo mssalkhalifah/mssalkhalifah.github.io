74582bde127ef741c625e96e9b586dd150d86215
mssalkhalifah <mssalkhalifah@gmail.com>
2022-03-06 14:41:28 +0300

    Changed to cloudinary image loader

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com'],
+    loader: 'cloudinary',
   },
   //basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   //assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
